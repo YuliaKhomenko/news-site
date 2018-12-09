496a4d7b99d213b1e0e8a0a4d9aa29e3b7ee7b3e
Aleksandr <mishenko.aleksandr@gmail.com>
2018-12-09 13:23:23 +0200

    add pictures displaying

--- src/Main/pictures/pictures.js
+++ src/Main/pictures/pictures.js
@@ -1,19 +1,19 @@
 const pictures = [
     {
         id:1,
-        image:"/images/articles/top.jpeg",
+        image:"/images/articles/top.jpg",
         name: '12 dead in California bar shooting',
     },
 
     {
         id:2,
-        image:"/images/articles/center.jpeg",
+        image:"/images/articles/center.jpg",
         name: '69-year-old Dutchman starts legal bid to become 20 years younger',
     },
 
     {
         id:3,
-        image:"/images/articles/foot.jpeg",
+        image:"/images/articles/foot.jpg",
         name: "Brazilian Grand Prix: Reliving Lewis Hamilton's first world title 10 years on",
     },
 ]
